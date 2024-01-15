53c0638d6a3cd7eead1c45ed32e3769f6c120133
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-01-16 01:10:05 +0800

    feat: add custom icons in font format for efficient usage

---
+++ lib/custom_icon.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcon {
+  CustomIcon._();
+
+  static const _kFontFam = 'CustomIcon';
+  static const String? _kFontPkg = null;
+
+  static const IconData buildingLine = IconData(0xe900, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData buildingSolid = IconData(0xe901, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData caretLineDown = IconData(0xe902, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData caretLineLeft = IconData(0xe903, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData caretLineRight = IconData(0xe904, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData caretLineUp = IconData(0xe905, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData flagLine = IconData(0xe906, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData flagSolid = IconData(0xe907, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heartLine = IconData(0xe908, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heartSolid = IconData(0xe909, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData homeLine = IconData(0xe90a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData homeSolid = IconData(0xe90b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData logoutLine = IconData(0xe90c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData logoutSolid = IconData(0xe90d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData poland = IconData(0xe90e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData searchLine = IconData(0xe90f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData userLine = IconData(0xe910, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData usersLine = IconData(0xe911, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData userSolid = IconData(0xe912, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData usersSolid = IconData(0xe913, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+
+}
