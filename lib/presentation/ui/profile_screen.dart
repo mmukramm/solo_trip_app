683cc3ecfa328db308ad680b27200f86f6e3f122
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-01-19 14:48:56 +0800

    feat: build profilepage ui

--- lib/presentation/ui/profile_screen.dart
+++ lib/presentation/ui/profile_screen.dart
@@ -1,14 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:solo_trip_app/common/colors_theme.dart';
+import 'package:solo_trip_app/custom_icon.dart';
+import 'package:solo_trip_app/helpers/app_size.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile Screen"),
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(32.0),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.vertical(
+                bottom: Radius.circular(20.0),
+              ),
+              gradient: primaryGradient,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.2),
+                  blurRadius: 4,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: AppSize.deviceWidth(context),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: primaryLightBackgroundColor,
+                        child: const CircleAvatar(
+                          radius: 28,
+                          backgroundImage:
+                              AssetImage("assets/images/profile.jpg"),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 24.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          "Ismail Ahmad Kanabawi",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium!
+                              .copyWith(
+                                fontSize: 24.0,
+                                color: primaryLightBackgroundColor,
+                              ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 24.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: SizedBox(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(12.0),
+                  backgroundColor: dangerColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(CustomIcon.logoutLine),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      "Logout",
+                      style: Theme.of(context).textTheme.displaySmall!.copyWith(
+                            color: primaryLightBackgroundColor,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
