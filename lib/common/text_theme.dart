54d6c27806071c669470cc076da8e6a9f94e5e81
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-01-19 14:48:42 +0800

    feat: add fontFamily in textTheme

--- lib/common/text_theme.dart
+++ lib/common/text_theme.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 TextTheme textTheme = const TextTheme(
-  displayLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
-  displayMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-  displaySmall: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-  headlineMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-  headlineSmall: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-  bodyLarge: TextStyle(fontSize: 20),
-  bodyMedium: TextStyle(fontSize: 16),
-  bodySmall: TextStyle(fontSize: 12),
-  labelLarge: TextStyle(fontSize: 8),
+  displayLarge: TextStyle(fontFamily: 'Montserrat', fontSize: 32.0, fontWeight: FontWeight.bold),
+  displayMedium: TextStyle(fontFamily: 'Montserrat', fontSize: 24.0, fontWeight: FontWeight.bold),
+  displaySmall: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0, fontWeight: FontWeight.bold),
+  headlineMedium: TextStyle(fontFamily: 'Montserrat', fontSize: 16, fontWeight: FontWeight.bold),
+  headlineSmall: TextStyle(fontFamily: 'Montserrat', fontSize: 12, fontWeight: FontWeight.bold),
+  bodyLarge: TextStyle(fontFamily: 'Montserrat', fontSize: 20),
+  bodyMedium: TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+  bodySmall: TextStyle(fontFamily: 'Montserrat', fontSize: 12),
+  labelLarge: TextStyle(fontFamily: 'Montserrat', fontSize: 8),
 );
