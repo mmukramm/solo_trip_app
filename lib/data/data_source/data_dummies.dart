7bdf94307ca1a01a935a2d29cff8b85b66478ee0
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-01-21 18:05:28 +0800

    feat: add data dummies

---
+++ lib/data/data_source/data_dummies.dart
@@ -0,0 +1,144 @@
+import 'package:solo_trip_app/data/models/Country.dart';
+
+List<Country> countryList = [
+  Country(
+    countryName: "Spain",
+    flag: "esp.png",
+    population: 47615034,
+    region: "Europe",
+    backdropImage: "spain_backdrop.jpg",
+    capital: "Madrid",
+    overview:
+        "A number of independent kingdoms united in 1492 to form the Kingdom of Spain, a cultural patchwork that continues to shape the modern nation’s dynamic identity. Spain comprises much of the Iberian Peninsula, which it shares with Portugal on the southwestern edge of Europe. It also includes the Balearic Islands in the Mediterranean Sea, the Canary Islands in the Atlantic Ocean and two enclaves in North Africa.",
+  ),
+  Country(
+    countryName: "Spain",
+    flag: "esp.png",
+    population: 47615034,
+    region: "Europe",
+    backdropImage: "spain_backdrop.jpg",
+    capital: "Madrid",
+    overview:
+        "A number of independent kingdoms united in 1492 to form the Kingdom of Spain, a cultural patchwork that continues to shape the modern nation’s dynamic identity. Spain comprises much of the Iberian Peninsula, which it shares with Portugal on the southwestern edge of Europe. It also includes the Balearic Islands in the Mediterranean Sea, the Canary Islands in the Atlantic Ocean and two enclaves in North Africa.",
+  ),
+  Country(
+    countryName: "New Zealand",
+    flag: "nz.png",
+    population: 5124100,
+    region: "Oceania",
+    backdropImage: "newzealand_backdrop.jpg",
+    capital: "Wellington",
+    overview:
+        "British and Polynesian influences course through picturesque New Zealand, an island nation in the Pacific Ocean southeast of Australia. Early Maori settlers ceded sovereignty to British invaders with the Treaty of Waitangi in 1840, and European settlers flooded in.",
+  ),
+  Country(
+    countryName: "Portugal",
+    flag: "port.png",
+    population: 10379007,
+    region: "Europe",
+    backdropImage: "portugal_backdrop.jpg",
+    capital: "Lisbon",
+    overview:
+        "Jutting into the Atlantic Ocean on the edge of the Iberian Peninsula and flanked by Spain to the east, Portugal is a nation with its gaze turned outward. Its history is steeped in discovery and exploration, beginning with early leaders that, after ousting Celtic and Moorish settlers in the 12th century, focused on building their kingdom beyond continental borders.",
+  ),
+  Country(
+    countryName: "Australia",
+    flag: "austrl.png",
+    population: 25978935,
+    region: "Oceania",
+    backdropImage: "sydney_backdrop.jpg",
+    capital: "Canberra",
+    overview:
+        "The Commonwealth of Australia occupies the Australian continent. The country also includes some islands, most notably Tasmania. Indigenous people occupied the land for at least 40,000 years before the first British settlements of the 18th century.",
+  ),
+  Country(
+    countryName: "Ireland",
+    flag: "irl.png",
+    population: 5086988,
+    region: "Europe",
+    backdropImage: "ireland_backdrop.jpg",
+    capital: "Dublin",
+    overview:
+        "The Republic of Ireland is an island nation in the Atlantic Ocean, separated from Britain on the east by the Irish Sea. Nicknamed the Emerald Isle for its well-watered grasslands, the country is known for its rich cultural traditions; in the Best Countries rankings the country routinely ranks high for being culturally accessible and friendly. The country comprises five-sixths of the island of Ireland – the remaining sixth is Northern Ireland, part of the United Kingdom.",
+  ),
+  Country(
+    countryName: "Switzerland",
+    flag: "switz.png",
+    population: 8769741,
+    region: "Europe",
+    backdropImage: "switzerland_backdrop.jpg",
+    capital: "Bern",
+    overview:
+        "Switzerland, officially called the Swiss Confederation, is a small country in Central Europe made up of 16,000 square miles of glacier-carved Alps, lakes and valleys. It is one of the world’s wealthiest countries, and has been well-known for centuries for its neutrality.",
+  ),
+  Country(
+    countryName: "Sweden",
+    flag: "swe.png",
+    population: 10486941,
+    region: "Europe",
+    backdropImage: "sweden_backdrop.jpg",
+    capital: "Stockholm",
+    overview:
+        "The Kingdom of Sweden, flanked by Norway to the west and the Baltic Sea to the east, expands across much of the Scandinavian Peninsula and is one of the largest countries in the European Union by land mass. Capital city Stockholm was founded in the 13th century, and border disputes through the Middle Ages established the modern-day nation.",
+  ),
+  Country(
+    countryName: "Norway",
+    flag: "nor.png",
+    population: 5457127,
+    region: "Europe",
+    backdropImage: "norway_backdrop.jpg",
+    capital: "Oslo",
+    overview:
+        "The Kingdom of Norway is the westernmost country in the Scandinavian peninsula, made up mostly of mountainous terrain. Nearly all of its population lives in the south, surrounding the capital, Oslo. Norway’s coastline is made up of thousands of miles of fjords, bays and island shores.",
+  ),
+  Country(
+    countryName: "Japan",
+    flag: "jap.png",
+    population: 125124989,
+    region: "Asia",
+    backdropImage: "japan_backdrop.jpg",
+    capital: "Tokyo",
+    overview:
+        "Japan, one of the world’s most literate and technically advanced nations, is an East Asian archipelago country made up of four primary islands and more than 6,800 others. While most of Japan is covered by mountains and heavily wooded areas, the country’s people lead a distinctly urban lifestyle. Long culturally influenced by its neighbors, today the country blends its ancient traditions with aspects of Western life.",
+  ),
+  Country(
+    countryName: "Finland",
+    flag: "fin.png",
+    population: 5556880,
+    region: "Europe",
+    backdropImage: "finland_backdrop.jpg",
+    capital: "Helsinki",
+    overview:
+        "Geography defines the history and culture of Nordic Finland, one of the most northern-reaching countries in the world. Bordered by Norway, Sweden, Russia, the Baltic Sea and the Gulf of Bothnia, Finland and its vast stretches of heavily forested open land acts as a northern gate between West and East.",
+  ),
+  Country(
+    countryName: "Indonesia",
+    flag: "idn.png",
+    population: 275501339,
+    region: "Asia",
+    backdropImage: "indonesia_backdrop.jpg",
+    capital: "Jakarta",
+    overview:
+        "Made up of a chain of thousands of islands between Asia and Australia, Indonesia is the world's largest archipelagic state and the world’s biggest Muslim-majority nation. Indonesia’s people are diverse, speaking more than 300 languages and ranging from cosmopolitan urbanites to rural villagers. There are hundreds of volcanoes in Indonesia, the most famous of which is Krakatoa, whose explosion in 1883 was one of the most catastrophic in history.",
+  ),
+  Country(
+    countryName: "Turkey",
+    flag: "tyrk.png",
+    population: 85341241,
+    region: "Middle East",
+    backdropImage: "turkey_backdrop.jpg",
+    capital: "Ankara",
+    overview:
+        "Founded in 1923, Turkey is home to a unique intersection of culture as the nation bridges Asia with Europe. The country is overwhelmingly Sunni Muslim, though influences of bygone Roman and Byzantine rule pepper the streets and skyline. Extravagant mosques and cathedrals can both be found within blocks of the Grand Bazaar in the city of Istanbul.",
+  ),
+  Country(
+    countryName: "Brazil",
+    flag: "braz.png",
+    population: 215313498,
+    region: "South America",
+    backdropImage: "brazil_backdrop.jpg",
+    capital: "Bras&iacute",
+    overview:
+        "Occupying half of South America's land mass, Brazil is the giant of the continent – both in size and in population. Brazil's history is filled with economic turmoil, veering from boom to bust, and its culture is a melting pot that has traditionally welcomed the world.",
+  ),
+];
---
+++ lib/data/models/country.dart
@@ -0,0 +1,19 @@
+class Country {
+  String countryName;
+  String flag;
+  int population;
+  String region;
+  String backdropImage;
+  String capital;
+  String overview;
+
+  Country({
+    required this.countryName,
+    required this.flag,
+    required this.population,
+    required this.region,
+    required this.backdropImage,
+    required this.capital,
+    required this.overview,
+  });
+}
